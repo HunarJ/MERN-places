fe658abbe277ea675e63d13d1c520f736e68ebb1
JanHunar <jan.hunar.jha@gmail.com>
2025-01-09 21:30:38 +0100

    zaloha

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {use, useCallback} from "react";
+
+import "./NewPlace.css";
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../util/validators";
 
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>
+const titleInputHandler = useCallback((id, value, isValid) => {
+
+}, [])
+
+  return (
+    <form className="place-form">
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please exter a valid title."
+        onInput={titleInputHandler}
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
